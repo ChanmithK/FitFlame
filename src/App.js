c064aade77117bc20d59debbca49db2c805b5995
Nimesha Muthukumarana <nimesha.muthukumarana@gmail.com>
2022-10-21 18:29:09 +0530

    sprint 02 - done

--- src/App.js
+++ src/App.js
@@ -1,81 +1,72 @@
-import { CssBaseline } from '@mui/material';
-import { Routes, Route, useLocation } from 'react-router-dom';
-import Blogs from './pages/Client/Main-Pages/Blogs';
-import Orders from './pages/Client/Main-Pages/Orders';
-import Schedules from './pages/Client/Main-Pages/Schedules';
-import TrainersList from './pages/Client/Main-Pages/Trainers-List';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { setTileName } from './redux/AppbarTitleSlice';
-import TrainerDetails from './pages/Client/Main-Pages/Trainer-Details';
-import MakeOrder from './pages/Client/Main-Pages/Make-Order';
-import ViewOrder from './pages/Client/Main-Pages/View-Order';
-import UpdateOrder from './pages/Client/Main-Pages/Update-Order';
-import ClientProfile from './pages/Client/Main-Pages/ClientProfile';
-import ClientProfileUpdate from './pages/Client/Main-Pages/ClientProfileUpdate';
-import BlogView from './pages/Client/Main-Pages/BlogView';
+import { CssBaseline } from '@mui/material'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import Blogs from './pages/Client/Main-Pages/Blogs'
+import Orders from './pages/Client/Main-Pages/Orders'
+import Schedules from './pages/Client/Main-Pages/Schedules'
+import TrainersList from './pages/Client/Main-Pages/Trainers-List'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { setTileName } from './redux/AppbarTitleSlice'
+import TrainerDetails from './pages/Client/Main-Pages/Trainer-Details'
+import MakeOrder from './pages/Client/Main-Pages/Make-Order'
+import ViewOrder from './pages/Client/Main-Pages/View-Order'
+import UpdateOrder from './pages/Client/Main-Pages/Update-Order'
+import ClientProfile from './pages/Client/Main-Pages/ClientProfile'
+import ClientProfileUpdate from './pages/Client/Main-Pages/ClientProfileUpdate'
+import BlogView from './pages/Client/Main-Pages/BlogView'
 
-import SignIn from './pages/User/Main-Pages/SignIn';
-import SignUp from './pages/User/Main-Pages/SignUp';
-import PasswordReset from './pages/User/Main-Pages/PasswordReset';
-import { UserAuthContextProvider } from './Context/UserAuthContext';
-import ProtectedRoute from './components/ProtectedRoute';
+import SignIn from './pages/User/Main-Pages/SignIn'
+import SignUp from './pages/User/Main-Pages/SignUp'
+import PasswordReset from './pages/User/Main-Pages/PasswordReset'
+import { UserAuthContextProvider } from './Context/UserAuthContext'
+import ProtectedRoute from './components/ProtectedRoute'
 
+import BlogList from './pages/Admin/MainPages/BlogList'
+import AdminUserList from './pages/Admin/MainPages/AdminUserList'
+import BlogUpdate from './pages/Admin/MainPages/BlogUpdate'
+import BlogViewAdmin from './pages/Admin/MainPages/BlogView'
+import BlogCreate from './pages/Admin/MainPages/BlogCreate'
+import Report from './pages/Admin/MainPages/Report'
+import WorkoutScheduleReport from './pages/Client/Main-Pages/Workout-Schedule-Report'
 
-
-
-
-
-
-
-import BlogList from "./pages/Admin/MainPages/BlogList";
-import AdminUserList from "./pages/Admin/MainPages/AdminUserList";
-import BlogUpdate from "./pages/Admin/MainPages/BlogUpdate";
-import BlogViewAdmin from "./pages/Admin/MainPages/BlogView";
-import BlogCreate from "./pages/Admin/MainPages/BlogCreate";
-import Report from "./pages/Admin/MainPages/Report";
-import WorkoutScheduleReport from "./pages/Client/Main-Pages/Workout-Schedule-Report";
-
-
-import TrainerClients from './pages/Trainer/MainPages/Clients';
-import TrainerOrders from './pages/Trainer/MainPages/Orders';
-import TrainerPackages from './pages/Trainer/MainPages/Packages';
-import TrainerOrderDetaills from './pages/Trainer/MainPages/OrderDetaills';
-import TrainerPlan from './pages/Trainer/MainPages/Plan';
-import TrainerViewPlan from './pages/Trainer/MainPages/ViewPlan';
-import TrainerAboutProfile from './pages/Trainer/MainPages/AboutProfile';
-import TrainerProfile from './pages/Trainer/MainPages/Profile';
-import TrainerProfileEdit from './pages/Trainer/MainPages/ProfileEdit';
-import TrainerCreatepackage from './pages/Trainer/MainPages/Createpackage';
-import TrainerUpdatePackage from './pages/Trainer/MainPages/UpdatePackage';
-import TrainerClientDetails from './pages/Trainer/MainPages/ClientDetails';
-import TrainerReport from './pages/Trainer/MainPages/Report';
-
-
+import TrainerClients from './pages/Trainer/MainPages/Clients'
+import TrainerOrders from './pages/Trainer/MainPages/Orders'
+import TrainerPackages from './pages/Trainer/MainPages/Packages'
+import TrainerOrderDetaills from './pages/Trainer/MainPages/OrderDetaills'
+import TrainerPlan from './pages/Trainer/MainPages/Plan'
+import TrainerViewPlan from './pages/Trainer/MainPages/ViewPlan'
+import TrainerAboutProfile from './pages/Trainer/MainPages/AboutProfile'
+import TrainerProfile from './pages/Trainer/MainPages/Profile'
+import TrainerProfileEdit from './pages/Trainer/MainPages/ProfileEdit'
+import TrainerCreatepackage from './pages/Trainer/MainPages/Createpackage'
+import TrainerUpdatePackage from './pages/Trainer/MainPages/UpdatePackage'
+import TrainerClientDetails from './pages/Trainer/MainPages/ClientDetails'
+import TrainerReport from './pages/Trainer/MainPages/Report'
+import MealPlanReport from './pages/Client/Main-Pages/MealPlanReport'
 
 export function App() {
-  const dispatch = useDispatch();
-  const location = useLocation();
+  const dispatch = useDispatch()
+  const location = useLocation()
 
   useEffect(() => {
     if (location.pathname === '/blogs') {
-      dispatch(setTileName('Blogs'));
+      dispatch(setTileName('Blogs'))
     } else if (location.pathname === '/trainers') {
-      dispatch(setTileName('Trainers'));
+      dispatch(setTileName('Trainers'))
     } else if (location.pathname === '/orders') {
-      dispatch(setTileName('Order History'));
+      dispatch(setTileName('Order History'))
     } else if (location.pathname === '/schedules') {
-      dispatch(setTileName('Schedules'));
+      dispatch(setTileName('Schedules'))
     } else if (location.pathname === `/trainer-details`) {
-      dispatch(setTileName('Trainer Details'));
+      dispatch(setTileName('Trainer Details'))
     } else if (location.pathname === '/make-order') {
-      dispatch(setTileName('Make Order'));
+      dispatch(setTileName('Make Order'))
     } else if (location.pathname === '/view-order') {
-      dispatch(setTileName('View Order'));
+      dispatch(setTileName('View Order'))
     } else if (location.pathname === '/update-order') {
-      dispatch(setTileName('Update Order'));
+      dispatch(setTileName('Update Order'))
     }
-  }, [location.pathname]);
+  }, [location.pathname])
 
   return (
     <div>
@@ -174,6 +165,7 @@ export function App() {
             path='/client-workout-schedule-report'
             element={<WorkoutScheduleReport />}
           />
+          <Route path='/client-meal-plan-report' element={<MealPlanReport />} />
 
           {/* Chamod */}
 
@@ -317,16 +309,15 @@ export function App() {
             }
           />
 
-          <Route path="/admin/blog/create" element={<BlogCreate />} />
-          <Route path="/admin/blog/view" element={<BlogViewAdmin />} />
-          <Route path="/admin/blog/update" element={<BlogUpdate />} />
-          <Route path="/admin/users" element={<AdminUserList />} />
-          <Route path="/admin/report" element={<Report />} />
-
+          <Route path='/admin/blog/create' element={<BlogCreate />} />
+          <Route path='/admin/blog/view' element={<BlogViewAdmin />} />
+          <Route path='/admin/blog/update' element={<BlogUpdate />} />
+          <Route path='/admin/users' element={<AdminUserList />} />
+          <Route path='/admin/report' element={<Report />} />
         </Routes>
       </UserAuthContextProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
